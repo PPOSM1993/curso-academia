fcb8a0717b9780784a0f8ecdf761a227a4c084dc
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-12-29 16:07:25 -0300

    Se trabaja en el Sidebar de el LMS

--- app/(routes)/(root)/page.tsx
+++ app/(routes)/(root)/page.tsx
@@ -1,11 +1,9 @@
 import { Button } from "@/components/ui/button";
-import { Header } from "./components/Header";
 
 export default function Home() {
   return (
     <>
       <div>
-        <Header />
         <Button>Boton</Button>
       </div>
     </>
