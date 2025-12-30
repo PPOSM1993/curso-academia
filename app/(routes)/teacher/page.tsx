8b564f7eb39623786dd87e3723d98429d9f2791c
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-12-30 14:22:37 -0300

    Se crea la pagina de profesor

--- app/(routes)/teacher/page.tsx
+++ app/(routes)/teacher/page.tsx
@@ -1,10 +1,20 @@
+import { currentUser } from '@clerk/nextjs/server'
 import React from 'react'
+import Header from './components/Header/Header';
 
-export default function TeacherPage() {
+export default async function TeacherPage() {
+    const user = await currentUser();
+    console.log(user);
+
+
+    if (!user) {
+        return <p>Not signed in</p>
+    }
+
     return (
         <>
             <div>
-                From Teacher Page
+                <Header/>
             </div>
         </>
     )
