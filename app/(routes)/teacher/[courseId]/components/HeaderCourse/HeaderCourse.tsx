bed9b3848d219591498868985193e00aa3725439
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2026-01-02 16:28:51 -0300

    Se corrige la vista de HeaderCourse

--- app/(routes)/teacher/[courseId]/components/HeaderCourse/HeaderCourse.tsx
+++ app/(routes)/teacher/[courseId]/components/HeaderCourse/HeaderCourse.tsx
@@ -4,11 +4,40 @@ import { HeaderCourseProps } from "./HeaderCourse.types";
 import { Eye, EyeOff, MoveLeft, Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { toast } from "sonner";
+import axios from "axios";
 
 export default function HeaderCourse(props: HeaderCourseProps) {
-    const { idCourse, idPublished } = props;
-    const { isLoading, setIsLoading } = useState(false);
-    const router = useRouter();
+    const router = useRouter()
+    const { idCourse, isPublished } = props
+    const [isLoading, setIsLoading] = useState(false)
+
+    const onPublish = async (state: boolean) => {
+        setIsLoading(true)
+        try {
+            axios.patch(`/api/course/${idCourse}`, {
+                isPublished: state
+            })
+            toast.success(state ? 'Curso publicado' : 'Curso despublicado')
+            router.refresh()
+
+        } catch (error) {
+            console.error(error)
+            toast.error('Error al publicar el curso')
+        }
+        setIsLoading(false)
+    }
+
+    const removeCourse = async () => {
+        try {
+            await axios.delete(`/api/course/${idCourse}`)
+            toast.success('Curso eliminado')
+            router.push('/teacher')
+        } catch (error) {
+            console.error(error)
+            toast.error('Error al eliminar el curso')
+        }
+    }
 
     return (
         <>
@@ -19,10 +48,11 @@ export default function HeaderCourse(props: HeaderCourseProps) {
                     </Button>
 
                     <div className='gap-2 flex items-center'>
-                        {idPublished ? (
+                        {isPublished ? (
                             <Button
                                 variant='outline'
                                 disabled={isLoading}
+                                onClick={() => onPublish(false)}
                             >
                                 No publicar
                                 <EyeOff />
@@ -30,6 +60,7 @@ export default function HeaderCourse(props: HeaderCourseProps) {
                         ) : (
                             <Button
                                 disabled={isLoading}
+                                onClick={() => onPublish(true)}
                             >
                                 Publicar
                                 <Eye />
@@ -37,6 +68,7 @@ export default function HeaderCourse(props: HeaderCourseProps) {
                         )}
                         <Button
                             variant='destructive'
+                            onClick={removeCourse}
                         >
                             <Trash />
                         </Button>
