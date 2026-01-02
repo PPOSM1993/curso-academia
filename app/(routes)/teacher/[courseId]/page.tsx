a56b3d9cd9b37f8fd9a52ddf1d87b5b61b13262e
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2026-01-02 15:33:40 -0300

    Se trabaja en la pagina o la vista del curso segun el Id

--- app/(routes)/teacher/[courseId]/page.tsx
+++ app/(routes)/teacher/[courseId]/page.tsx
@@ -1,9 +1,55 @@
+import { auth } from '@clerk/nextjs/server'
+import prisma from '@/lib/prisma'
+import { HeaderCourse } from './components';
 
-export default function CoursePage() {
+export default async function CoursePage({
+    params
+}: {
+    params: Promise<{ courseId: string }>
+}) {
+
+    const { courseId } = await params;
+    const { userId } = await auth();
+
+
+    if (!userId) {
+        return (
+            <div className='px-6'>
+                <div className='w-full bg-white my-2 p-3 rounded-md'>
+                    <p>No tienes permisos para ver este curso.</p>
+                </div>
+            </div>
+        )
+    }
+
+    const course = await prisma.course.findUnique({
+        where: {
+            id: courseId,
+            userId
+        },
+        include: {
+            chapters: {
+                orderBy: {
+                    position: 'asc'
+                }
+            }
+        }
+    })
+
+    if (!course) {
+        return (
+            <div className='px-6'>
+                <div className='w-full bg-white my-2 p-3 rounded-md'>
+                    <p>El curso no existe.</p>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <>
             <div className='m-6'>
-                xD
+               <HeaderCourse idCourse={course.id} isPublished={course.isPublished} />
             </div>
         </>
     )
