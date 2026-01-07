297339c36c5d292620ef3f7cc38aea69b6fabc7f
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2026-01-07 14:52:16 -0300

    Se avanza en el formulario de Chapter Block

--- app/(routes)/teacher/[courseId]/components/ChapterBlock/FormChapterName.tsx
+++ app/(routes)/teacher/[courseId]/components/ChapterBlock/FormChapterName.tsx
@@ -1,16 +1,69 @@
 "use client";
 
-import { FormChapterNameProps } from "./FormChapterName.types";
-import { formSchema } from "./FormChapterName.form";
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 
+import { useRouter } from 'next/navigation'
+import axios from 'axios'
+import { toast } from 'sonner'
+
+import { Button } from '@/components/ui/button'
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+import { formSchema } from './FormChapterName.form'
+
+import { FormChapterNameProps } from './FormChapterName.types'
 
 export default function FormChapterName(props: FormChapterNameProps) {
-    const { idCourse, setShowInputChapter } = props
+    const { idCourse, setShowInputChapter } = props;
+
+    const router = useRouter()
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: ''
+        }
+    })
+
+    // 2. Define a submit handler.
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        try {
+
+        } catch (error) {
+            toast.error('Error al crear el capítulo')
+            console.error(error)
+        }
+    }
+
     return (
         <>
-            <div>
-
-            </div>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4 mb-4'>
+                    <FormField
+                        control={form.control}
+                        name='title'
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input placeholder='Ej: Introduccón a la programación.' {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type='submit' disabled={!form.formState.isValid}>Crear</Button>
+                </form>
+            </Form>
         </>
     )
 }
