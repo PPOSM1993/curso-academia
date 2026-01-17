16299a99756ca7762c239d6be6fe644918c615eb
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2026-01-17 14:38:27 -0300

    Se trabaja en la pagina principal de el sitio de academia

---
+++ app/(routes)/(root)/components/ExploreCourse/ExploreCourse.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Search } from "lucide-react"
+import { useRouter } from "next/navigation"
+import Image from "next/image"
+
+export function ExploreCourse() {
+  const router = useRouter()
+
+  return (
+    <div className="my-4 mx-6 border rounded-lg bg-white gap-4">
+      <div className="flex items-center justify-between gap-8 p-6 w-full">
+        
+        {/* TEXTO */}
+        <div className="flex flex-col gap-4 max-w-2xl">
+          <h1 className="text-4xl font-semibold">
+            Explora todos los Cursos
+          </h1>
+
+          <p className="text-balance max-w-2xl">
+            Empieza a aprender a programar desde cero con estos cursos.
+            No necesitas experiencia ni conocimientos previos,
+            no necesitas un ordenador de última tecnología.
+            Solo necesitas muchas ganas y un buen café.
+          </p>
+
+          <Button
+            className="w-fit flex gap-2"
+            onClick={() => router.push("/courses")}
+          >
+            Explora todos los Cursos
+            <Search className="w-5 h-5" />
+          </Button>
+        </div>
+
+        {/* IMAGEN */}
+        <div className="flex-shrink-0">
+          <Image
+            src="/explore2.svg"
+            alt="Todos los Cursos"
+            width={340}
+            height={240}
+            className="object-contain"
+          />
+        </div>
+
+      </div>
+    </div>
+  )
+}
--- app/(routes)/(root)/page.tsx
+++ app/(routes)/(root)/page.tsx
@@ -1,19 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { ExploreCourse } from "./components";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="text-center text-white px-6">
-        <h1 className="text-5xl font-extrabold tracking-tight mb-4 text-black">
-          Bienvenido a tu LMS
-        </h1>
-
-        <p className="text-lg text-black max-w-xl mx-auto mb-8">
-          Aprende, progresa y alcanza tus objetivos paso a paso.
-        </p>
-
-
-      </div>
+    <div>
+      <ExploreCourse />
     </div>
   );
 }
