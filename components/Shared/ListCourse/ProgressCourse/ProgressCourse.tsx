ec937fde1b1c9dafabc0405e4dcd98ee0c3ad08f
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2026-01-23 18:11:39 -0300

    Avance en el formato del precio del curso

--- components/Shared/ListCourse/ProgressCourse/ProgressCourse.tsx
+++ components/Shared/ListCourse/ProgressCourse/ProgressCourse.tsx
@@ -2,6 +2,8 @@
 
 import { currentUser } from "@clerk/nextjs/server";
 import { ProgressCourseProps } from "./ProgressCourse.type"
+import { getUserProgressCourse } from "@/actions/getUserProgressCourse";
+import { Progress } from "@/components/ui/progress";
 
 export async function ProgressCourse(props: ProgressCourseProps) {
 
@@ -16,9 +18,30 @@ export async function ProgressCourse(props: ProgressCourseProps) {
         )
     }
 
+    const progressCourse = await getUserProgressCourse(user.id, courseId);
+
+    console.log("progressCourse", progressCourse);
+
     return (
         <>
-            xd
+            <div className="mt-4">
+                <div className="bg-violet-100 rounded-full w-full px-4">
+                    <Progress value={50} className="[&>*]:bg-violet-300" />
+                    <p className="text-xs mt-1">{progressCourse}% Completado</p>
+                </div>
+                {totalChapters > 0 && progressCourse > 0 ? (
+                    <>
+                        <div>
+                            <Progress value={50} className="[&>*]:bg-violet-300" />
+                            <p className="text-xs mt-1">{progressCourse}% Completado</p>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                    <h4>${price} CLP</h4>
+                    </>
+                )}
+            </div>
         </>
     )
 }
